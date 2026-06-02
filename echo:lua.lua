edd4a5e36c0389f9e66574f77eba8b00df95bf03
slop <mehmetalieren69@gmail.com>
2026-06-02 13:34:49 +0300

    echo.lua aracı eklendi.

---
+++ echo:lua.lua
@@ -0,0 +1,23 @@
+local fcntl = require("posix.fcntl")
+local unistd = require("posix.unistd")
+local inputstring = ""
+local start_idx = 1
+local append_newline = true
+
+local function printing()
+    for i = start_idx, #arg do
+    unistd.write(unistd.STDOUT_FILENO, arg[i] .. " ")
+    if i == #arg then
+
+    end
+    if append_newline == true then
+        unistd.write(unistd.STDOUT_FILENO, "\n")
+        end
+end
+if arg[1] == "-n" then
+    start_idx = 2
+    append_newline = false
+    printing()
+else
+    printing()
+end
