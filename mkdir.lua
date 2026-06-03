eb309a9b4b5ab74398f9a58f72d6575bd8f8ee9e
slop <mehmetalieren69@gmail.com>
2026-06-03 19:03:48 +0300

    mkdir aracı tamamlandı.

--- mkdir.lua
+++ mkdir.lua
@@ -1,9 +1,11 @@
 local stat = require("posix.sys.stat")
 local fcntl = require("posix.fcntl")
 local unistd = require("posix.unistd")
-local default_mode = "0o755"
+local errno = require("posix.errno")
+local default_mode = 493
 local mode = 0
-
+local opt_verbose = false
+local opt_parents = false
 local function print_help()
     local help_text =[[
         Kullanım: mkdir [SEÇENEK]... DİZİN...
@@ -21,9 +23,10 @@ local function print_help()
         --version
         output version information and exit
     ]]
-    unistd.write(unistd.STDOUT_FILENO, help_text)
+    unistd.write(unistd.STDOUT_FILENO, help_text .. "\n")
     os.exit(0)
     end
+
 local dirs = {}
 local i = 1
 
@@ -31,22 +34,70 @@ while i <= #arg do
     if arg[i] == "-h" or arg[i] == "--help" then
         print_help()
         os.exit(0)
-        elseif arg[i] == "-v" or arg[i] == "--version" then
-            unistd.write(unistd.STDOUT_FILENO, "mkdir:lua Version1.0")
+        elseif arg[i] == "--version" then
+            unistd.write(unistd.STDOUT_FILENO, "mkdir:lua Version 1.0\n")
             os.exit(0)
                 elseif arg[i] == "-m" or arg[i] == "--mode" then
                     mode = tonumber(arg[i+1], 8)
-                    i = i + 2
+                        i = i + 2
+                    elseif arg[i] == "-v" or arg[i] == "--verbose" then
+                        opt_verbose = true
+                        i = i + 1
+                        elseif arg[i] == "-p" or arg[i] == "--parents" then
+                            opt_parents = true
+                            i = i + 1
                     else
 
                         table.insert(dirs, arg[i])
                         i = i + 1
                         end
                         end
+
+if #dirs == 0 then
+    unistd.write(unistd.STDERR_FILENO, "mkdir: eksik işlenen\nDaha fazla bilgi için 'mkdir --help' deneyin.\n")
+    os.exit(1)
+    end
 if mode == 0 then
     mode = default_mode
-end
+    end
 
-for
+for i = 1, #dirs do
+   if opt_parents then
+        local current_path = ""
+
+        if string.sub(dirs[i], 1, 1) == "/" then
+            current_path = ""
+        end
 
 
+        for part in string.gmatch(dirs[i], "[^/]+") do
+            if current_path == "" and string.sub(dirs[i], 1, 1) ~= "/" then
+                current_path = part
+            else
+                current_path = current_path .. "/" .. part
+            end
+
+            local success,err,errnum = stat.mkdir(current_path, mode)
+            if success then
+                if opt_verbose then
+                    unistd.write(unistd.STDOUT_FILENO, "mkdir: created directory: " .. current_path .. "\n")
+                end
+            else
+                if errnum ~= errno.EEXIT then
+                    unistd.write(unistd.STDOUT_FILENO, "mkdir: Error:" .. " " .. err .. "\n")
+                    break
+
+                end
+            end
+        end
+        else
+            local success,err,errnum = stat.mkdir(dirs[i], mode)
+                if not success then
+                    unistd.write(unistd.STDERR_FILENO, "mkdir: Error:" .. " " .. err .. "\n")
+                else
+                    if opt_verbose then
+                        unistd.write(unistd.STDOUT_FILENO, "mkdir: created directory: " .. dirs[i] .. "\n")
+                    end
+                end
+        end
+        end
